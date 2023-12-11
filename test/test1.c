18608a22f34e1875ade298faf9f068ab7c9e7c9d
MMUWEI <1821370389@qq.com>
2023-12-11 09:52:47 +0800

    [修改] 添加了注释

--- test/test1.c
+++ test/test1.c
@@ -1,5 +1,6 @@
 /* 2023.12.9 */
 /* A，B in list；A+B=C */
+/* 给定一个数组，找出两个数字之和等于目标值的下标,假定结果最多有一组 */
 #include <stdio.h>
 
 #define ARRAY_SIZE 5
@@ -24,9 +25,12 @@ int arraySum(int *array, int tonget, int size, int *outcome)
 
 int main()
 {
-    int array[ARRAY_SIZE];
+    /* 数组 */
+    int array[ARRAY_SIZE] = {0};
+    /* 目标值 */
     int tonget ;
-    printf("请输入数组(5个数)：\n");
+    /* 设定数组和目标值*/
+    printf("请输入数组(%d个数)：\n",ARRAY_SIZE);
     for(int idx = 0; idx < ARRAY_SIZE; idx++)
     {
         printf("请输入第%d个数：", idx+1);
@@ -34,6 +38,8 @@ int main()
     }
     printf("请输入目标值：");
     scanf("%d", &tonget);
+
+    /* 输出结果 */
     int outcome[2];
     if (arraySum(array, tonget, sizeof(array)/sizeof(array[0]), outcome))
     {
