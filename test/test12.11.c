bf996c09383acad191e4d3a9adbf643ae9aff017
MMUWEI <1821370389@qq.com>
2023-12-11 20:00:49 +0800

    [新增] 新增数组去重

--- test/test12.11.c
+++ test/test12.11.c
@@ -8,20 +8,21 @@
 
 int main()
 {
+#if 1
     int array[ARRAYSIZE] = {0};
 
     /* 随机数组 */
     srand(time(NULL));
     for (int idx = 0; idx < ARRAYSIZE; idx++)
     {
-        array[idx] = rand() % 100 + 1;
+        array[idx] = rand() % 20 + 1;
     }
 
     /* 冒泡排序 */
     int temp = 0;
     for(int idx = 0; idx < ARRAYSIZE; idx++)
     {
-        for(int jdx = 0; jdx < ARRAYSIZE-idx ; jdx++)
+        for(int jdx = 0; jdx < ARRAYSIZE ; jdx++)
         {
             if(array[idx] < array[jdx])
             {
@@ -39,4 +40,47 @@ int main()
     }
     printf("\n");
 
+#endif
+
+/* 数组去重 */
+#if 0
+    /* 测量新数组的长度 */
+    int count = 0;
+    for(int idx = 0; idx < ARRAYSIZE; idx++)
+    {
+        if(array[idx] != array[idx+1])
+        {
+            count++;
+        }
+    }
+    /* 创建新数组 */
+    int *Array2 = (int *)malloc(count * sizeof(int));
+    count = 0;
+    for(int idx = 0; idx < ARRAYSIZE; idx++)
+    {
+        if(array[idx] != array[idx+1])
+        {
+            Array2[count] = array[idx];
+            count++;
+        }
+    }
+    
+    /* 打印新数组 */
+    for(int idx = 0; idx < count; idx++)
+    {
+        printf("%d ", Array2[idx]);  //打印新数组的长度  
+    }
+    printf("\n");
+#else
+    /* 如果不需要新数组的话*/
+    for(int idx = 0; idx < ARRAYSIZE; idx++)
+    {
+        if(array[idx] != array[idx+1])
+        {
+            printf("%d ", array[idx]);
+        }
+    }
+    printf("\n");
+
+#endif
 }
