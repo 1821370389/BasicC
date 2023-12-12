22d51e54a36e83f5391583ff29434549a37f2bb2
MMUWEI <1821370389@qq.com>
2023-12-12 09:36:24 +0800

    [新增] 新增makefile的demo

--- demoMakefile/main.c
+++ demoMakefile/main.c
@@ -2,23 +2,8 @@
 #include <stdlib.h>
 
 
-int printString()
-{
-    int ret = 0;
-    printf("print string\n");
-    return ret;
-}
 int main()
 {
-    int num1 = 10;
-    printf("num1 = %d\n", num1);
-    printString();
+    printf("Hello World!\n");
     return 0;
 }
-/*
-编译过程
-gcc -E main .c >main.i
-gcc -S main.i
-gcc -c main.s
-gcc main.o 
-*/
